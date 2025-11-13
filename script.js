b0f1ecf5a636c1746c66b189cd49b4c2c93463b0
salmankhan8822 <salmankhan03159416118@gmail.com>
2025-11-13 10:12:09 +0500

    create project

---
+++ script.js
@@ -0,0 +1,16 @@
+let button = document.getElementById("backtotop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    button.classList.add("show");
+  } else {
+    button.classList.remove("show");
+  }
+});
+
+button.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
